8bfacdf97ddf420a4b2c2875e17b8cffa1a60c80
Phong Huynh <phuynh@architech.ca>
2016-03-18 14:59:33 -0400

    Add validation constraints

--- registration-server/src/main/java/ca/architech/model/User.java
+++ registration-server/src/main/java/ca/architech/model/User.java
@@ -4,20 +4,28 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Entity
 public class User {
+    public static final int USERNAME_MIN_LENGTH = 5;
+    public static final int USERNAME_MAX_LENGTH = 50;
+    public static final int PASSWORD_MIN_LENGTH = 8;
+    public static final int PASSWORD_MAX_LENGTH = 50;
+
     @GeneratedValue
     @Id
     private Long id;
 
+    @Pattern(regexp = "^[A-Za-z0-9]*$")
     @NotNull
-    @Size(min = 5, max = 30, message = "Username must at least 5 characters.")
+    @Size(min = USERNAME_MIN_LENGTH, max = USERNAME_MAX_LENGTH, message = "Username must at least 5 characters.")
     private String username;
 
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]$")
     @NotNull
-    @Size(min = 8, max = 30, message = "Password must at least 8 characters.")
+    @Size(min = PASSWORD_MIN_LENGTH, max = PASSWORD_MAX_LENGTH, message = "Password must at least 8 characters.")
     private String password;
 
     public User() { }
