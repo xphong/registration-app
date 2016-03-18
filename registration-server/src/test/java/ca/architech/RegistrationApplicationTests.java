df7604f660c5ebfebdf42cc247a866009e2af235
Phong Huynh <phuynh@architech.ca>
2016-03-18 11:19:03 -0400

    Rebuild spring app

--- registration-server/src/test/java/ca/architech/RegistrationApplicationTests.java
+++ registration-server/src/test/java/ca/architech/RegistrationApplicationTests.java
@@ -1,14 +1,12 @@
-package ca.architech.registration;
+package ca.architech;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = RegistrationApplication.class)
-@WebAppConfiguration
 public class RegistrationApplicationTests {
 
 	@Test
