a5751e1cab11cc0e512b1fda5afc3fd823833af3
Phong Huynh <phuynh@architech.ca>
2016-03-21 14:53:53 -0400

    Add registration validation

--- registration-web/src/app/home/home.ts
+++ registration-web/src/app/home/home.ts
@@ -1,7 +1,8 @@
 import {Component} from 'angular2/core';
-import {FORM_DIRECTIVES} from 'angular2/common';
+import {FORM_DIRECTIVES, FormBuilder, Validators, Control, ControlGroup} from 'angular2/common';
 
 import {RegistrationService} from '../shared/services/registration';
+import {RegistrationValidator} from '../shared/validation/registrationvalidator';
 
 @Component({
   selector: 'home',
@@ -16,13 +17,39 @@ import {RegistrationService} from '../shared/services/registration';
   template: require('./home.html')
 })
 export class Home {
-  username = '';
-  password = '';
+  form: ControlGroup;
+  username: Control;
+  password: Control;
+
   errorMessage = '';
   successMessage = '';
 
-  constructor(public registrationService: RegistrationService) {
+  constructor(public registrationService: RegistrationService, public formBuilder: FormBuilder) {
+    this.username = new Control(
+      '', 
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(50),
+        RegistrationValidator.startsWithNumber,
+        RegistrationValidator.alphaNumericValues
+      ])
+    );
 
+    this.password = new Control(
+      '',
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(50),
+        RegistrationValidator.validatePassword
+      ])
+    );
+
+    this.form = formBuilder.group({
+      username:  this.username,
+      password: this.password
+    });
   }
 
   ngOnInit() {
@@ -30,12 +57,8 @@ export class Home {
   }
 
   register() {
-    let user = {
-      'username': this.username,
-      'password': this.password
-    };
-
-    this.registrationService.registerUser(user)
+    console.log(this.form.value);
+    this.registrationService.registerUser(this.form.value)
         .subscribe(data => {
           if (data) {
             this.errorMessage = '';
---
+++ registration-web/src/app/shared/validation/registrationvalidator.ts
@@ -0,0 +1,35 @@
+import { Control } from "angular2/common";
+
+interface ValidationResult {
+  [key: string]: boolean;
+}
+
+export class RegistrationValidator {
+  static startsWithNumber(control: Control): ValidationResult {
+    if (control.value != '' && !isNaN(control.value.charAt(0))) {
+      return {'startsWithNumber': true};
+    }
+
+    return null;
+  }
+
+  static alphaNumericValues(control: Control): ValidationResult {
+    let regex = /^[A-Za-z0-9]*$/;
+
+    if (!regex.test(control.value)) {
+      return {'alphaNumericValues': true };
+    }
+
+    return null;
+  }
+
+  static validatePassword(control: Control): ValidationResult {
+    let regex = /^.*(?=.*\d)(?=.*[a-zA-Z]).*$/;
+
+    if (!regex.test(control.value)) {
+      return { 'validatePassword': true };
+    }
+
+    return null;
+  }
+}
