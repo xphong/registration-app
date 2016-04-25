56073ec81f8b5de672e56748ae4d7beab1dff25e
Phong Huynh <phuynh@architech.ca>
2016-04-25 16:12:44 -0400

    Add tests for login and register

--- registration-web/src/app/login/login.spec.ts
+++ registration-web/src/app/login/login.spec.ts
@@ -4,29 +4,44 @@ import {
   injectAsync,
   describe,
   beforeEachProviders,
-  TestComponentBuilder
+  TestComponentBuilder,
+  resetBaseTestProviders,
+  setBaseTestProviders
 } from 'angular2/testing';
 
-import {Component, provide} from 'angular2/core';
-import {BaseRequestOptions, Http} from 'angular2/http';
-import {MockBackend} from 'angular2/http/testing';
-import {RegistrationService} from '../shared/services/registration';
+import {TEST_BROWSER_PLATFORM_PROVIDERS, TEST_BROWSER_APPLICATION_PROVIDERS} from 'angular2/platform/testing/browser';
+import {Observable} from 'rxjs/Rx';
+import {provide} from 'angular2/core';
+import {RootRouter} from 'angular2/src/router/router';
+import {Location, Router, RouteRegistry, ROUTER_PRIMARY_COMPONENT} from 'angular2/router';
+import {SpyLocation} from 'angular2/src/mock/location_mock';
 
+import {RegistrationService} from '../shared/services/registration';
 import {Login} from './login';
+import {App} from '../app';
+
+class MockRegistrationService {
+  loginUser(user) {
+    return Observable.of({
+      username: 'TestUser1',
+      password: 'TestPassword1'
+    });
+  }
+}
 
 describe('Login', () => {
+  resetBaseTestProviders();
+  setBaseTestProviders(TEST_BROWSER_PLATFORM_PROVIDERS, TEST_BROWSER_APPLICATION_PROVIDERS);
+
+  let registrationService = new MockRegistrationService();
 
   beforeEachProviders(() => [
-    BaseRequestOptions,
-    MockBackend,
-    RegistrationService,
-    provide(Http, {
-      useFactory: function(backend, defaultOptions) {
-        return new Http(backend, defaultOptions);
-      },
-      deps: [MockBackend, BaseRequestOptions]
-    }),
-    Login
+    Login,
+    RouteRegistry,
+    provide(RegistrationService, { useValue: registrationService }),
+    provide(Location, {useClass: SpyLocation}),
+    provide(Router, {useClass: RootRouter}),
+    provide(ROUTER_PRIMARY_COMPONENT, {useValue: App})
   ]);
 
   it('should log ngOnInit', inject([Login], (login) => {
@@ -36,5 +51,23 @@ describe('Login', () => {
     login.ngOnInit();
     expect(console.log).toHaveBeenCalled();
   }));
+
+  it('should open', injectAsync([TestComponentBuilder], (tcb) => {
+      return tcb
+        .createAsync(Login)
+        .then(fixture => {
+          let loginComponent = fixture.componentInstance;
+
+          fixture.detectChanges();
+
+          loginComponent.login({
+            username: 'TestUser1',
+            password: 'TestPassword1'
+          });
+
+          expect(loginComponent.successMessage).toEqual('Login successful');
+          expect(loginComponent.errorMessage).toEqual('');
+        });
+    }));
 
 });
--- registration-web/src/app/register/register.spec.ts
+++ registration-web/src/app/register/register.spec.ts
@@ -4,29 +4,44 @@ import {
   injectAsync,
   describe,
   beforeEachProviders,
-  TestComponentBuilder
+  TestComponentBuilder,
+  resetBaseTestProviders,
+  setBaseTestProviders
 } from 'angular2/testing';
 
-import {Component, provide} from 'angular2/core';
-import {BaseRequestOptions, Http} from 'angular2/http';
-import {MockBackend} from 'angular2/http/testing';
-import {RegistrationService} from '../shared/services/registration';
+import {TEST_BROWSER_PLATFORM_PROVIDERS, TEST_BROWSER_APPLICATION_PROVIDERS} from 'angular2/platform/testing/browser';
+import {Observable} from 'rxjs/Rx';
+import {provide} from 'angular2/core';
+import {RootRouter} from 'angular2/src/router/router';
+import {Location, Router, RouteRegistry, ROUTER_PRIMARY_COMPONENT} from 'angular2/router';
+import {SpyLocation} from 'angular2/src/mock/location_mock';
 
+import {RegistrationService} from '../shared/services/registration';
 import {Register} from './register';
+import {App} from '../app';
+
+class MockRegistrationService {
+  registerUser(user) {
+    return Observable.of({
+      username: 'TestUser1',
+      password: 'TestPassword1'
+    });
+  }
+}
 
 describe('Register', () => {
+  resetBaseTestProviders();
+  setBaseTestProviders(TEST_BROWSER_PLATFORM_PROVIDERS, TEST_BROWSER_APPLICATION_PROVIDERS);
+
+  let registrationService = new MockRegistrationService();
 
   beforeEachProviders(() => [
-    BaseRequestOptions,
-    MockBackend,
-    RegistrationService,
-    provide(Http, {
-      useFactory: function(backend, defaultOptions) {
-        return new Http(backend, defaultOptions);
-      },
-      deps: [MockBackend, BaseRequestOptions]
-    }),
-    Register
+    Register,
+    RouteRegistry,
+    provide(RegistrationService, { useValue: registrationService }),
+    provide(Location, {useClass: SpyLocation}),
+    provide(Router, {useClass: RootRouter}),
+    provide(ROUTER_PRIMARY_COMPONENT, {useValue: App})
   ]);
 
   it('should log ngOnInit', inject([Register], (register) => {
@@ -36,5 +51,23 @@ describe('Register', () => {
     register.ngOnInit();
     expect(console.log).toHaveBeenCalled();
   }));
+
+  it('should open', injectAsync([TestComponentBuilder], (tcb) => {
+      return tcb
+        .createAsync(Register)
+        .then(fixture => {
+          let registerComponent = fixture.componentInstance;
+
+          fixture.detectChanges();
+
+          registerComponent.register({
+            username: 'TestUser1',
+            password: 'TestPassword1'
+          });
+
+          expect(registerComponent.successMessage).toEqual('Account successfully created');
+          expect(registerComponent.errorMessage).toEqual('');
+        });
+    }));
 
 });
