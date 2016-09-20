d7d395233a5d86e6fc9a8904150705b6a0c1a38a
Phong Huynh <phuynh@architech.ca>
2016-09-20 12:10:41 -0400

    Fix app test

--- registration-web/src/app/app.spec.ts
+++ registration-web/src/app/app.spec.ts
@@ -1,80 +1,22 @@
 import {
-  it,
   inject,
-  injectAsync,
-  beforeEach,
-  beforeEachProviders,
-  TestComponentBuilder
-} from 'angular2/testing';
+  TestBed
+} from '@angular/core/testing';
 
-import { RootRouter } from 'angular2/src/router/router';
-import { Location, RouteParams, Router, RouteRegistry, ROUTER_PRIMARY_COMPONENT } from 'angular2/router';
-import { SpyLocation } from 'angular2/src/mock/location_mock';
-import { provide } from 'angular2/core';
-
-import { App } from './app';
+import { App } from './app.component';
+import { AppState } from './app.service';
 
 describe('App', () => {
 
-  beforeEachProviders(() => [
-    App
-  ]);
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      AppState,
+      App
+    ]
+  }));
 
   it('should have an app title', inject([ App ], (app) => {
     expect(app.title).toEqual('Registration App');
   }));
-
-});
-
-describe('Router', () => {
-
-  let location, router;
-
-  beforeEachProviders(() => [
-    RouteRegistry,
-    provide(Location, {useClass: SpyLocation}),
-    provide(Router, {useClass: RootRouter}),
-    provide(ROUTER_PRIMARY_COMPONENT, {useValue: App})
-  ]);
-
-  beforeEach(inject([Router, Location], (_router, _location) => {
-    router = _router;
-    location = _location;
-  }));
-
-  it('Should be able to navigate to Home', done => {
-    router.navigate(['Home']).then(() => {
-      expect(location.path()).toBe('');
-      done();
-    }).catch(e => done.fail(e));
-  });
-
-  it('should redirect not registered urls to Home', done => {
-    router.navigateByUrl('/unknown').then(() => {
-      expect(location.path()).toBe('');
-      done();
-    }).catch(e => done.fail(e));
-  });
-
-  it('Should be able to navigate to About', done => {
-    router.navigate(['About']).then(() => {
-      expect(location.path()).toBe('/about');
-      done();
-    }).catch(e => done.fail(e));
-  });
-
-  it('Should be able to navigate to Login', done => {
-    router.navigate(['Login']).then(() => {
-      expect(location.path()).toBe('/login');
-      done();
-    }).catch(e => done.fail(e));
-  });
-
-  it('Should be able to navigate to Register', done => {
-    router.navigate(['Register']).then(() => {
-      expect(location.path()).toBe('/register');
-      done();
-    }).catch(e => done.fail(e));
-  });
 
 });
