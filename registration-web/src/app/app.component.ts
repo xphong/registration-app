3c5a826d05db73f5949501c70c744028bb0e38cf
Phong Huynh <phuynh@architech.ca>
2016-09-20 11:26:13 -0400

    Add styling

--- registration-web/src/app/app.component.ts
+++ registration-web/src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app',
   encapsulation: ViewEncapsulation.None,
+  styleUrls: [
+    './app.style.css'
+  ],
   templateUrl: './app.html'
 })
 export class App {
